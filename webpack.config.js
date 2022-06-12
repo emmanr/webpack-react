b80bc1522954a57c8c9827ae28836fec592710f0
emmanr <emman.info22@gmail.com>
2022-06-12 22:44:38 +0800

    Created webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  devtool:false,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+};
